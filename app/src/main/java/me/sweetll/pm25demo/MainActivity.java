d7eb285faca9119c6299c9856dae55605f80776c
blackbbc <505968815@qq.com>
2015-09-08 23:28:51 +0800

    share to weixin

--- app/src/main/java/me/sweetll/pm25demo/MainActivity.java
+++ app/src/main/java/me/sweetll/pm25demo/MainActivity.java
@@ -2,8 +2,13 @@ package me.sweetll.pm25demo;
 
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -15,6 +20,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
@@ -35,20 +41,23 @@ import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 import org.w3c.dom.Text;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.Date;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import me.relex.circleindicator.CircleIndicator;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GooeyMenu.GooeyMenuInterface {
     @Bind(R.id.drawer_layout) DrawerLayout mDrawer;
     @Bind(R.id.toolbar) Toolbar toolbar;
     @Bind(R.id.dynamicArcView) DecoView arcView;
     @Bind(R.id.pm_num) TextView pm_num_view;
     @Bind(R.id.healthy_status) TextView healthy_view;
-
-
+    @Bind(R.id.gooey_menu) GooeyMenu gooeyMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +69,8 @@ public class MainActivity extends AppCompatActivity {
         final ActionBar ab = getSupportActionBar();
         ab.setHomeAsUpIndicator(getResources().getDrawable(R.drawable.ic_menu));
         ab.setDisplayHomeAsUpEnabled(true);
+
+        gooeyMenu.setOnMenuListener(this);
 
         ViewPager chartViewpager = (ViewPager) findViewById(R.id.viewpager_chart);
         CircleIndicator chartIndicator = (CircleIndicator) findViewById(R.id.indicator_chart);
@@ -136,8 +147,13 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_map:
+                Intent intent = new Intent(this, MapActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_settings:
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -147,4 +163,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
     }
+
+    @Override
+    public void menuOpen() {
+
+    }
+
+    @Override
+    public void menuClose() {
+
+    }
+
+    public File getScreenShot() {
+        Date now = new Date();
+        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+
+        try {
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/PM2.5/" + now + ".jpg";
+            View view = getWindow().getDecorView().getRootView();
+            view.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+            view.setDrawingCacheEnabled(false);
+
+            File imageFile = new File(mPath);
+
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            return imageFile;
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+        return null;
+
+    }
+
+    @Override
+    public void menuItemClicked(int menuNumber) {
+//        Toast.makeText(this, "" + menuNumber, Toast.LENGTH_SHORT).show();
+        switch (menuNumber) {
+            case 1:
+                Intent shareIntent = new Intent();
+                shareIntent.setComponent(new ComponentName("com.tencent.mm", "com.tencent.mm.ui.tools.ShareToTimeLineUI"));
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.setType("image/*");
+                shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                shareIntent.putExtra("Kdescription", "This is a demo");
+                Uri uri = Uri.fromFile(getScreenShot());
+                shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+                startActivity(shareIntent);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+    }
+
 }
